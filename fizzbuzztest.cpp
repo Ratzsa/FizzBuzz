457ec8f5afd8bf85c7f77b7fd6dabdda813457de
Daniel Wagenius <wagenius.daniel@gmail.com>
2023-04-21 18:10:44 +0200

    first commit

---
+++ fizzbuzztest.cpp
@@ -0,0 +1,72 @@
+#include <gtest/gtest.h>
+#include <stdbool.h>
+
+extern "C"
+{
+    #include "fizzbuzz.h"
+}
+
+class FizzBuzzTest : public testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        // 
+    }
+};
+
+TEST_F(FizzBuzzTest,WhenTalIsDivisibleBy3And5ThenFizzBuzzShouldBeReturned)
+{
+    int tal[] = { 15, 30, 45, 75, 90 };
+    char buffer[10];
+    bool testSuccess = true;
+    for(int i = 0; i < sizeof(tal) / sizeof(tal[0]); i++)
+    {
+        calculateFizzBuzz(tal[i], buffer);
+        if(strcmp("FizzBuzz", buffer) != 0)
+        {
+            testSuccess = false;
+        }
+    }
+    ASSERT_FALSE(!testSuccess);
+}
+
+TEST_F(FizzBuzzTest,WhenTalIsDivisibleBy3ThenFizzShouldBeReturned)
+{
+    int tal[] = { 3, 6, 9, 18, 36 };
+    char buffer[10];
+    bool testSuccess = true;
+    for(int i = 0; i < sizeof(tal) / sizeof(tal[0]); i++)
+    {
+        calculateFizzBuzz(tal[i], buffer);
+        if(strcmp("Fizz", buffer) != 0)
+        {
+            testSuccess = false;
+        }
+    }
+    ASSERT_FALSE(!testSuccess);
+}
+
+TEST_F(FizzBuzzTest,WhenTalIsDivisibleBy5ThenBuzzShouldBeReturned)
+{
+    int tal[] = { 5, 10, 25, 35, 85 };
+    char buffer[10];
+    bool testSuccess = true;
+    for(int i = 0; i < sizeof(tal) / sizeof(tal[0]); i++)
+    {
+        calculateFizzBuzz(tal[i], buffer);
+        if(strcmp("Buzz", buffer) != 0)
+        {
+            testSuccess = false;
+        }
+    }
+    ASSERT_FALSE(!testSuccess);
+}
+
+TEST_F(FizzBuzzTest,WhenTalIsNotDivisibleBy3Or5ThenBuzzShouldBeReturned)
+{
+    int tal = 7;
+    char buffer[10];
+    calculateFizzBuzz(tal, buffer);
+    ASSERT_STREQ("7", buffer);
+}
